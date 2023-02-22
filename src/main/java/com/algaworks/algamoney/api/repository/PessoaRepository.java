60c1f613fb82673cb391a5ef1656c46816a488f9
Denilson Vitorino <dv22pro@gmail.com>
2023-02-21 23:53:26 -0300

    7.7. Desafio: Pesquisa de pessoa

--- src/main/java/com/algaworks/algamoney/api/repository/PessoaRepository.java
+++ src/main/java/com/algaworks/algamoney/api/repository/PessoaRepository.java
@@ -1,11 +1,16 @@
 package com.algaworks.algamoney.api.repository;
 
 import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.algaworks.algamoney.api.model.Pessoa;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-	Optional<Pessoa> findByCodigo(Long codigo);
+	Optional<Pessoa> findByCodigo(Long codigo);	
+	public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 }
